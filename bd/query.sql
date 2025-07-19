b82cdc576d27b71ded3cf5808bc36eb320204705
josune-mo <josune.mo@gmail.com>
2025-07-19 10:59:14 +0200

    Update

--- bd/query.sql
+++ bd/query.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.42, for macos15 (arm64)
 --
 -- Host: localhost    Database: netflix
 -- ------------------------------------------------------
--- Server version	8.0.42
+-- Server version	9.3.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,7 +29,7 @@ CREATE TABLE `actors` (
   `country` varchar(45) NOT NULL,
   `birthday` date DEFAULT NULL,
   PRIMARY KEY (`idActor`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,7 +38,35 @@ CREATE TABLE `actors` (
 
 LOCK TABLES `actors` WRITE;
 /*!40000 ALTER TABLE `actors` DISABLE KEYS */;
+INSERT INTO `actors` VALUES (1,'Tom','Hanks','Estaods Unidos','1956-07-09'),(2,'Roberto','Benigni','Italia','1952-10-27'),(3,'John','Travolta','Estaods Unidos','1954-02-18');
 /*!40000 ALTER TABLE `actors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `movie_actors`
+--
+
+DROP TABLE IF EXISTS `movie_actors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `movie_actors` (
+  `idMovie` int NOT NULL,
+  `idActor` int NOT NULL,
+  PRIMARY KEY (`idMovie`,`idActor`),
+  KEY `idActor` (`idActor`),
+  CONSTRAINT `movie_actors_ibfk_1` FOREIGN KEY (`idMovie`) REFERENCES `movies` (`idMovie`),
+  CONSTRAINT `movie_actors_ibfk_2` FOREIGN KEY (`idActor`) REFERENCES `actors` (`idActor`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `movie_actors`
+--
+
+LOCK TABLES `movie_actors` WRITE;
+/*!40000 ALTER TABLE `movie_actors` DISABLE KEYS */;
+INSERT INTO `movie_actors` VALUES (3,1),(2,2),(1,3);
+/*!40000 ALTER TABLE `movie_actors` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -56,7 +84,7 @@ CREATE TABLE `movies` (
   `category` varchar(45) NOT NULL,
   `year` int DEFAULT NULL,
   PRIMARY KEY (`idMovie`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -65,7 +93,36 @@ CREATE TABLE `movies` (
 
 LOCK TABLES `movies` WRITE;
 /*!40000 ALTER TABLE `movies` DISABLE KEYS */;
+INSERT INTO `movies` VALUES (1,'Pulp Fiction','Crimen','https://pics.filmaffinity.com/pulp_fiction-210382116-large.jpg','Top 10',1994),(2,'La vita è bella','Comedia','https://pics.filmaffinity.com/la_vita_e_bella-646167341-mmed.jpg','Top 10',1997),(3,'Forrest Gump','Comedia','https://pics.filmaffinity.com/forrest_gump-212765827-mmed.jpg','Top 10',1994);
 /*!40000 ALTER TABLE `movies` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_favorites`
+--
+
+DROP TABLE IF EXISTS `user_favorites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_favorites` (
+  `idUser` int NOT NULL,
+  `idMovie` int NOT NULL,
+  `score` tinyint DEFAULT NULL,
+  PRIMARY KEY (`idUser`,`idMovie`),
+  KEY `idMovie` (`idMovie`),
+  CONSTRAINT `user_favorites_ibfk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`idUser`),
+  CONSTRAINT `user_favorites_ibfk_2` FOREIGN KEY (`idMovie`) REFERENCES `movies` (`idMovie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_favorites`
+--
+
+LOCK TABLES `user_favorites` WRITE;
+/*!40000 ALTER TABLE `user_favorites` DISABLE KEYS */;
+INSERT INTO `user_favorites` VALUES (1,1,9),(1,2,8);
+/*!40000 ALTER TABLE `user_favorites` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -83,7 +140,7 @@ CREATE TABLE `users` (
   `email` varchar(45) NOT NULL,
   `planDetails` varchar(45) NOT NULL,
   PRIMARY KEY (`idUser`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -92,6 +149,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'laura_dev','laura','Laura','laura@gmail.com','Standard'),(3,'ester_dev','ester','Ester','ester@gmail.com','Standard');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -104,4 +162,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-07-12 10:04:03
+-- Dump completed on 2025-07-19  8:25:28
